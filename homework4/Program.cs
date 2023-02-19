b10cb2fe609ce388e00013be35c201784b44abef
Oleg <shuka.21@mail.ru>
2023-02-19 14:00:53 +0300

    add homework4

---
+++ homework4/Program.cs
@@ -0,0 +1,19 @@
+﻿int i = 1;
+int a;
+bool not = true;
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + a);
+while (i <= a)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+}
+
+if (not)
+{
+Console.WriteLine("Нет чётных чисел!");
+}
