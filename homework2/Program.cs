e1ccc8bf35f440c757cd17e47447b974e6ab06d2
Oleg <shuka.21@mail.ru>
2023-02-19 12:55:50 +0300

    Add homework2

---
+++ homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿int a, b, c;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+{
+    
+        if (a >= b && a >= c)
+            max = a;
+           
+        else if (b >= a && b >= c)
+            max = b;
+        
+        else if (c >= a && c >= b)
+            max = c;
+    
+}
+        Console.WriteLine("max = "+ max);
